0f2efb10eb2d3f7cd2545e2d8bf1126d9321ebe4
savayer <sava0094@gmail.com>
2021-03-01 20:55:28 +0200

    (upd) updated package.json & lib file. Added editorconfig

--- src/lib.js
+++ src/lib.js
@@ -1,2 +1,7 @@
 import VueHoverSlider from "@/components/VueHoverSlider";
-export default VueHoverSlider
+
+export default {
+	install (Vue) {
+		Vue.component('vue-hover-slider', VueHoverSlider)
+	}
+}
